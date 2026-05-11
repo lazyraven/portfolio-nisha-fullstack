61dc17753bc71fcbc327631fcb2998e6eab88b93
Nisha Yadav <nishayadav1703@gmail.com>
2026-05-11 16:29:33 +0530

    small change in ite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  // base: '/',
   // base: '/portfolio-nisha-fullstack/',
   plugins: [react()],
 });
